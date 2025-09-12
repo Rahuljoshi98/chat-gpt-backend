2d0487127101dc8cb9791d3dc87cea62fce2fe3e
Rahuljoshi98 <2000rahuljoshi@gmail.com>
2025-09-13 00:05:32 +0530

    cors fixed

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://your-frontend-domain.vercel.app",
+      "https://chat-gpt-frontend-chi.vercel.app",
     ],
     credentials: true,
   }),
