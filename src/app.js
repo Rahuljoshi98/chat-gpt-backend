74fc8c829287da99d586085f38a4bd6d91b09ef1
Rahuljoshi98 <2000rahuljoshi@gmail.com>
2025-09-14 01:50:40 +0530

    get auth added

--- src/app.js
+++ src/app.js
@@ -7,6 +7,18 @@ import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://chat-gpt-frontend-chi.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
 // ✅ Middlewares
 app.use(express.json());
 app.use(clerkMiddleware());
@@ -23,18 +35,6 @@ app.use(express.static("public"));
 //     credentials: true,
 //   }),
 // );
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://chat-gpt-frontend-chi.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
 
 // import routes
 import routes from "./routes/index.js";
